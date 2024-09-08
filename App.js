5648e4ebbf6978c24ebfd4ea42d9f9d543a26edb
cweek <105405328+cweek@users.noreply.github.com>
2024-09-08 18:42:55 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,260 @@
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import './App.css';
+
+const tokenFaucetABI = [
+    {
+      "type": "constructor",
+      "inputs": [
+        {
+          "name": "_initialSupply",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable"
+    },
+    {
+      "type": "function",
+      "name": "DECIMALS",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8",
+          "internalType": "uint8"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "REQUEST_AMOUNT",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "REQUEST_COOLDOWN",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "balanceOf",
+      "inputs": [
+        {
+          "name": "",
+          "type": "address",
+          "internalType": "address"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "lastRequestTime",
+      "inputs": [
+        {
+          "name": "",
+          "type": "address",
+          "internalType": "address"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "name",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "string",
+          "internalType": "string"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "requestTokens",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool",
+          "internalType": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable"
+    },
+    {
+      "type": "function",
+      "name": "symbol",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "string",
+          "internalType": "string"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "totalSupply",
+      "inputs": [],
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "transfer",
+      "inputs": [
+        {
+          "name": "recipient",
+          "type": "address",
+          "internalType": "address"
+        },
+        {
+          "name": "amount",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool",
+          "internalType": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable"
+    },
+    {
+      "type": "event",
+      "name": "TokenRequested",
+      "inputs": [
+        {
+          "name": "user",
+          "type": "address",
+          "indexed": true,
+          "internalType": "address"
+        },
+        {
+          "name": "amount",
+          "type": "uint256",
+          "indexed": false,
+          "internalType": "uint256"
+        }
+      ],
+      "anonymous": false
+    },
+    {
+      "type": "event",
+      "name": "Transfer",
+      "inputs": [
+        {
+          "name": "from",
+          "type": "address",
+          "indexed": true,
+          "internalType": "address"
+        },
+        {
+          "name": "to",
+          "type": "address",
+          "indexed": true,
+          "internalType": "address"
+        },
+        {
+          "name": "value",
+          "type": "uint256",
+          "indexed": false,
+          "internalType": "uint256"
+        }
+      ],
+      "anonymous": false
+    }
+  ];
+
+const contractAddress = '0x5e139a17d501F278e7B5C1913d5841cDa9F8788e';
+
+const TokenFaucetRequest = () => {
+  const [userAddress, setUserAddress] = useState('');
+  const [message, setMessage] = useState('');
+
+  const requestTokens = async () => {
+    if (!window.ethereum) {
+      setMessage('MetaMask is not installed!');
+      return;
+    }
+
+    try {
+      // Request access the user's MetaMask account
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      
+      // Create instance of smart contract
+      const tokenFaucet = new ethers.Contract(contractAddress, tokenFaucetABI, signer);
+      
+      // Call requestTokens function from smart contract
+      const tx = await tokenFaucet.requestTokens();
+      setMessage('Transaction submitted! Waiting for confirmation...');
+      
+      // Wait for transaction to be confirmed
+      await tx.wait();
+      setMessage('Tokens successfully requested!');
+    } catch (error) {
+      console.error('Error requesting tokens:', error);
+      setMessage(`Error: ${error.reason}`);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Chewy Test Tokens Faucet</h1>
+      <button onClick={requestTokens}>Request Tokens</button>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default TokenFaucetRequest;
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# test-token-faucet
+# Chewy Test Tokens Faucet
+
+## Overview
+The Chewy Test Tokens (CHEWTT) Faucet dApp allows a user to connect their MetaMask wallet and request tokens from a smart contract. A wallet address can  request 0.05 CHEWTT every 24 hours. This github repo includes the frontend code for this dApp and the smart contract it is connected to.
+
+## Prerequisites
+- Node.js
+- npm
+- MetaMask [browser extension](https://metamask.io/download/)
+
+## Installation
+1. Once you have cloned the repository, run `cd test-token-faucet`
+2. Run `npm install` to install dependencies
+
+## Run the dApp
+1. `npm start`
